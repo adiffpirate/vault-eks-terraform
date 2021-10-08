b62a15a64e7cdc3c19cd99f4a46e214b5dffad1b
adiffpirate <lpsmonteiro@gmail.com>
2021-10-08 14:48:02 -0300

    [Security] Enabled encryption at rest

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -20,14 +20,17 @@ data "vault_generic_secret" "static_db_creds" {
 }
 
 resource "aws_db_instance" "mysql" {
-  identifier          = var.database.identifier
-  name                = var.database.name
+  identifier = var.database.identifier
+  name       = var.database.name
+
   instance_class      = "db.t3.micro"
   allocated_storage   = "5"
   engine              = "mysql"
   username            = data.vault_generic_secret.static_db_creds.data["username"]
   password            = data.vault_generic_secret.static_db_creds.data["password"]
+
   skip_final_snapshot = true
+  storage_encrypted   = true
   
   publicly_accessible    = true # Potential security risk, use with caution
   vpc_security_group_ids = [aws_security_group.mysql.id]
